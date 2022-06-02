7e415f1380c74fa6489e9c6398f5f7d226de70cf
MubarekHussen <mubahussen2014@gmail.com>
2022-06-02 23:27:09 +0300

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-print(f'{a} + {b} = {add(a,b)}')
+
+
+def add_function():
+
+    a = 1
+    b = 2
+    print(f'{a} + {b} = {add(a,b)}')
+
+
+if __name__ == "__main__":
+    add_function()
