936281e0f7f9ccac9779cbbd5b333f93c68ebeaf
MubarekHussen <mubahussen2014@gmail.com>
2022-06-16 15:23:05 +0300

    advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
+for letter in range(122, 96, -1):
+    if letter % 2 == 1:
+        letter = letter - 32
+    print("{:c}".format(letter), end='')
