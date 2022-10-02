dccf84886d55b503d11292f02ca03a8d679ae520
MubarekHussen <mubahussen2014@gmail.com>
2022-10-03 02:20:50 +0300

    trying

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,23 @@
-#!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable email must be sent with the value hr@holbertonschool.com. A variable subject must be sent with the value I will always be here for PLD
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
